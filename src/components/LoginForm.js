3b6de99ce6fa28a444bafe4cc9de996f0684f69b
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-05-25 22:14:19 +0900

    begin work on log in page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const LoginForm = () => {
+  const authedUser = useSelector((state) => state.authedUser);
+  console.log(authedUser);
+
+  //we need a handle submit function that sets the authedUser state
+  //and redirects to the dashboard
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("authedUser: " + authedUser);
+  };
+
   return (
     <div className="center">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <Link to="/dashboard">
-          <button className="btn" type="submit">
-            Login
-          </button>
-        </Link>
+        {/* <Link to="/dashboard"> */}
+        <button className="btn" type="submit">
+          Login
+        </button>
+        {/* </Link> */}
       </form>
     </div>
   );
