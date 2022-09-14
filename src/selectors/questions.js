d96fd8ef161353a5fa62d2a7c8c942cf2e01d4e0
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-09-14 09:49:22 +0900

    ensure that invalid question:id url returns a 404 page

--- src/selectors/questions.js
+++ src/selectors/questions.js
@@ -1,9 +1,16 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { QUESTION_ANSWER_TYPE } from "../utils/enums";
 
-const getQuestions = ({questions}) => questions.questions
+export const getQuestions = ({ questions }) => questions.questions
 
+export const getAllQuestion = () => createSelector(getQuestions, (questions) => questions)
+
+// export const getQuestionById = (id) => createSelector(getQuestions, (questions) => questions[id] != undefined || questions[id] != null ? questions[id] : console.log(questions))
 export const getQuestionById = (id) => createSelector(getQuestions, (questions) => questions[id])
+
+// export const getQuestionById = (id) =>
+// console.log(id)
+// createSelector(getQuestions, (questions) => questions[id])
 
 export const getPollCounts = (id) => createSelector(getQuestionById(id), (question) => {
   return {
--- src/views/PollPage.js
+++ src/views/PollPage.js
@@ -6,9 +6,16 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
+import { getAllQuestion } from "../selectors/questions";
+import { useParams } from "react-router-dom";
+
+import Error from "../views/404"
+
 const PollPage = () => {
+  const { id } = useParams();
   const navigate = useNavigate()
   const user = useSelector((state) => state.authedUser);
+  const questions = useSelector(getAllQuestion());
 
   useEffect(() => {
     user.authedUser != null ?
@@ -20,9 +27,29 @@ const PollPage = () => {
   return (
     <div className="poll-page-plus">
       <Navbar />
-      <PollQuestion />
+
+      {/* {
+        Object.keys(questions).map((v, i) => v == id ?
+
+          <PollQuestion key={i} />
+
+          : null
+
+        )} */}
+
+      {questions[id] !== undefined ? <PollQuestion /> : <Error />}
     </div>
   );
 };
 
 export default PollPage;
+
+// <div className="not-found">
+//   <h2>404</h2>
+//   <h3>Sorry, that page does not exist.</h3>
+//   <div className="not-found-btn">
+//     <div onClick={handleClick}>
+//       <button className="not-found-btn">Back to Home</button>
+//     </div>
+//   </div>
+// </div>
